3b56927f5bcc360510b264a5b9ddc4ea6d7be8a5
Brock Whitten <brock@chloi.io>
2015-04-09 14:48:36 -0700

    wrote first test to ensure lib exists

---
+++ test/basic.js
@@ -0,0 +1,11 @@
+var exhale = require("../")
+var should = require("should")
+
+describe("basic", function(){
+
+  it("should exist", function(done){
+    should.exist(exhale)
+    done()
+  })
+
+})
