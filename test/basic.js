781247912ff77b60e6c5e79b3a650ca272b8cd33
Brock Whitten <brock@chloi.io>
2015-04-09 15:00:39 -0700

    has get, set, del methods

--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,4 @@
-var exhale = require("../")
+var exhale = require("../")()
 var should = require("should")
 
 describe("basic", function(){
@@ -7,5 +7,14 @@ describe("basic", function(){
     should.exist(exhale)
     done()
   })
+
+  it("should have Create Read Delete methods", function(done){
+    exhale.should.have.property("get")
+    exhale.should.have.property("set")
+    exhale.should.have.property("del")
+    done()
+  })
+
+
 
 })
